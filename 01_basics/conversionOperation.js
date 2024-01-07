31a713e8366c877db71e450ad4ee6d2a6302dd6e
Nitul Das <112264960+Nitull@users.noreply.github.com>
2024-01-07 19:47:15 +0000

    conversion of datatypes

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,10 @@
+let score = true
+//console.log(typeof score);
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+let isLoggedIn = 1
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanisLoggedIn);
+console.log(booleanisLoggedIn);
