2d18a11e61b363698ce3a15a228575dd961c5f69
Nitul Das <112264960+Nitull@users.noreply.github.com>
2024-01-07 16:35:17 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+let score = 66
+let age = "16"
+let isFollow = true
+console.log(typeof isFollow);
+console.log(typeof null)
+console.log(typeof undefined);
+console.log(3+3)
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
