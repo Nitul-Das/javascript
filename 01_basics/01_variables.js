9b2394213fdf0737b3c39a3416f3017b885e4d71
Nitul Das <112264960+Nitull@users.noreply.github.com>
2024-01-07 16:32:21 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 1234
+let accountEmail = "nitul@gmail.com"
+var accountPassword = "123"
+accountCity = "guwahati"
+
+accountEmail = "das@gmail.com"
+accountPassword = "234"
+accountCity = "nalbari";
+
+console.log([accountId, accountEmail, accountPassword, accountCity]);
+console.table([accountId, accountEmail, accountPassword, accountCity])
