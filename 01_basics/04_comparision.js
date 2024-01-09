32de48790c1d2406480245517a170d7485972cd5
Nitul Das <112264960+Nitull@users.noreply.github.com>
2024-01-09 14:56:55 +0000

    comparision of dataypes

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,11 @@
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+//three equal strictly checking
+
+console.log("2" ===2);
