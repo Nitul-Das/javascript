8750479951cff8c16e6baee220a307a21a1bc95a
Nitul Das <112264960+Nitull@users.noreply.github.com>
2024-01-11 20:00:51 +0000

    numbers and maths

---
+++ 01_basics/06_number_and_math.js
@@ -0,0 +1,18 @@
+const score = 34
+console.log(score);
+
+const balance = new Number(46)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber = 123.8966
+console.log(otherNumber.toPrecision(3));
+
+const hundreds = 10000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+//++++++++++++++++++maths+++++++++++++++++//
+
+
