5d7fbb281112be9cf3957cf2d98989d1370c785d
Nitul Das <112264960+Nitull@users.noreply.github.com>
2024-01-23 19:12:05 +0000

    array

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -2,4 +2,20 @@ const marvel_heroes = ["thor", "ironman", "spiderman"]
 const dc_heroes = ["flash", "batman", "superman"]
 
 const all_new_heroes = [...marvel_heroes, ...dc_heroes]
-console.log(all_new_heroes);
+//console.log(all_new_heroes);
+
+
+let myArray = [1, 2, 3, [4, 5], 6, 7, [8, 9]]
+let anotherArray = myArray.flat(Infinity)
+//console.log(anotherArray);
+
+console.log(Array.isArray("Nitul"))
+console.log(Array.from("Nitul"))
+console.log(Array.from({name: "Nitul"})) // interesting
+
+let score1 = 10
+let score2 = 20
+let score3 = 30
+
+console.log(Array.of(score1, score2, score3));
+
