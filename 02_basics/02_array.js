4675cff437c267e3c39cc85e319bc7bbd12d263e
Nitul Das <112264960+Nitull@users.noreply.github.com>
2024-01-23 19:02:33 +0000

    arrays

---
+++ 02_basics/02_array.js
@@ -0,0 +1,5 @@
+const marvel_heroes = ["thor", "ironman", "spiderman"]
+const dc_heroes = ["flash", "batman", "superman"]
+
+const all_new_heroes = [...marvel_heroes, ...dc_heroes]
+console.log(all_new_heroes);
