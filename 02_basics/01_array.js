993499dabe8011443e144882691cbfbaf8916535
Nitul Das <112264960+Nitull@users.noreply.github.com>
2024-01-23 16:56:21 +0000

    arrays

---
+++ 02_basics/01_array.js
@@ -0,0 +1,9 @@
+let myArr = [ 0, 1, 2, 3, 4, 5]
+//console.log(myArr[0]);
+
+//myArr.push(6)
+//myArr.pop()
+//myArr.unshift(9)
+//myArr.shift()
+console.log(myArr.includes(9));
+console.log(myArr.indexOf(9));
