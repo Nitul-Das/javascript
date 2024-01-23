307550d9c01d1702542163acf0f4e7bc0a717b87
Nitul Das <112264960+Nitull@users.noreply.github.com>
2024-01-23 20:04:34 +0000

    array

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -5,5 +5,22 @@ let myArr = [ 0, 1, 2, 3, 4, 5]
 //myArr.pop()
 //myArr.unshift(9)
 //myArr.shift()
-console.log(myArr.includes(9));
-console.log(myArr.indexOf(9));
+//console.log(myArr.includes(9));
+//console.log(myArr.indexOf(9));
+// console.log(myArr.indexOf(3));
+
+//const newArr = myArr.join()
+
+ //console.log(myArr);
+ //console.log( newArr);
+
+
+console.log("A", myArr);
+
+let myn1 = myArr.slice(1, 3)
+console.log(myn1);
+console.log("B", myArr);
+
+let myn2 = myArr.splice(1, 3)
+console.log(myn2);
+console.log("c", myArr);
