6d680175af011f26da9c9f9a6edfbcf5c751b13c
Nitul Das <112264960+Nitull@users.noreply.github.com>
2024-01-30 18:46:27 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,15 @@
+function sayMyName(){
+    console.log("N");
+    console.log("i");
+    console.log("t");
+    console.log("u");
+    console.log("l");
+}
+
+sayMyName()
+
+function addTwoNumbers(number1, number2){
+    console.log(number1 + number2);
+}
+
+addTwoNumbers(8, 8)
