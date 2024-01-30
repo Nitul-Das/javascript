e28850254d72bbd96419efaed8c926fd5a86ab7b
Nitul Das <112264960+Nitull@users.noreply.github.com>
2024-01-30 15:43:26 +0000

    json talks

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -46,8 +46,35 @@ const users = [
 users[1].email
 //console.log(tinderUser);
 
-console.log(Object.keys(tinderUser)); // to access the keys
-console.log(Object.values(tinderUser)); // to access the values of keys
-console.log(Object.entries(tinderUser));
+//console.log(Object.keys(tinderUser)); // to access the keys
+//console.log(Object.values(tinderUser)); // to access the values of keys
+//console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+//console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "nitul"
+}
+
+//console.log(course.courseInstructor);
+
+const {courseInstructor: inst} = course  // destructuring
+//console.log(courseInstructor);
+console.log(inst);
+
+//JSON
+
+
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
