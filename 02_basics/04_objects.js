97af22396f64ce6152f5bc04f35c1f6a8864f640
Nitul Das <112264960+Nitull@users.noreply.github.com>
2024-01-30 06:39:45 +0000

    objects

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,53 @@
+//const tinderUser = new Object()  // singleton object
+const tinderUser = {}   // non-singleton object
+
+tinderUser.id = "123abc"
+tinderUser.name = "nitul"
+tinderUser.isLoggedIn = false
+
+//console.log(tinderUser);
+
+const regularUser = {
+    email: "nitul@gmail.com",
+
+        fullName:{
+            userfullname:{
+                 
+                firstName: "nitul",
+                lastName: "das",
+        }
+
+    }
+}
+
+//console.log(regularUser.fullName.userfullname.firstName);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+
+const obj3 = {...obj1,...obj2}
+//console.log(obj3);
+
+const users = [
+    {
+        id: 1,
+        email: "nit@gmail.com"
+    },
+    {
+        id: 1,
+        email: "das@gmail.com"
+    },
+    {
+        id: 1,
+        email: "neooh@gmail.com"
+    },
+]
+
+users[1].email
+//console.log(tinderUser);
+
+console.log(Object.keys(tinderUser)); // to access the keys
+console.log(Object.values(tinderUser)); // to access the values of keys
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
