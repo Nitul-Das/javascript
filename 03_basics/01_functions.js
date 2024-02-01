f3f54142ac04fffddb0fa62374aefd0ce3facf9f
Nitul Das <112264960+Nitull@users.noreply.github.com>
2024-02-01 19:49:51 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,15 +1,28 @@
-function sayMyName(){
-    console.log("N");
-    console.log("i");
-    console.log("t");
-    console.log("u");
-    console.log("l");
-}
+//function sayMyName(){
+//    console.log("N");
+//    console.log("i");
+//    console.log("t");
+//    console.log("u");
+//    console.log("l");
+//}
 
-sayMyName()
+//sayMyName()
+
+//function addTwoNumbers(number1, number2){
+  //  console.log(number1 + number2);
+//}
 
 function addTwoNumbers(number1, number2){
-    console.log(number1 + number2);
+        return number1 + number2              // to store in variable we use return
+}                                             // after return we cannot execute 
+
+let result = addTwoNumbers(8, 8)
+console.log("result:", result);
+
+function loginUserMessage(username){
+    return `${username} is logged in `
 }
 
-addTwoNumbers(8, 8)
+console.log(loginUserMessage("nitul"));
+console.log(loginUserMessage());  // when we dont give any value it shows undefined
+
