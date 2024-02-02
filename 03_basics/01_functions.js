7cbfc300043cf69e27b351201d819db670b633a7
Nitul Das <112264960+Nitull@users.noreply.github.com>
2024-02-02 13:56:48 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -19,10 +19,44 @@ function addTwoNumbers(number1, number2){
 let result = addTwoNumbers(8, 8)
 console.log("result:", result);
 
-function loginUserMessage(username){
+function loginUserMessage(username="sam"){
+    
+   if(!username){
+    console.log("please enter a username");
+    return
+   }
+
+
     return `${username} is logged in `
 }
 
 console.log(loginUserMessage("nitul"));
-console.log(loginUserMessage());  // when we dont give any value it shows undefined
+console.log(loginUserMessage("das"));  // when we dont give any value it shows undefined
 
+function calculateCartPrice(val1,val2,...numb1){
+  return numb1
+}
+
+console.log(calculateCartPrice(200, 300, 4000));
+
+
+const user = {
+  name: "nitul",
+  price: 199,
+
+}
+
+function handleObject(anyobject){
+  console.log(`name is ${anyobject.name} price is ${anyobject.price}`);
+}
+
+handleObject(user)
+
+
+const myNewArray =[200, 400, 300]
+
+function returnSecondValue(getarray){
+  return getarray[1]
+}
+
+console.log(returnSecondValue(myNewArray));
