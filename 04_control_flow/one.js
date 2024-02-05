c99ba717627ecda2a9d22f1ba21d6ee220ce8f5b
Nitul Das <112264960+Nitull@users.noreply.github.com>
2024-02-05 16:45:48 +0000

    control_flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,24 @@
+const isUserLoggedIn = true
+
+//     >, <, <=, >=, == (equal),!=  (not equal)    //
+
+//    *strict checking*   //
+//    === (also checks data type),!==       // 
+
+const temperature = 50
+
+//if(temperature === 50){
+
+//    console.log("temperature is less then 50");
+//}
+
+// else{
+//   console.log("temperature is more then 50");
+// } 
+
+const score = 200
+
+if(score > 100) {
+    const power = "fly"
+    console.log(`User power: ${power}`);
+}
