8301ce7cc454edac82a0ae94c2e24629efc07d5e
Nitul Das <112264960+Nitull@users.noreply.github.com>
2024-02-07 16:15:02 +0000

    high order array loops

---
+++ 05_iterations/three.js
@@ -0,0 +1,22 @@
+// for off
+
+// let arr = [1, 2, 3, 4, 5]
+
+// for (const num of arr) {
+//     console.log(num);
+    
+// }
+
+//
+
+const greetings = "hello world!"
+for (const greet of greetings){
+    if(greet === ' '){
+        
+        continue
+    }
+    console.log(greet);
+}
+
+
+
