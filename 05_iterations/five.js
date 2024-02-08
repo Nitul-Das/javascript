28cfc0eb790b59c8692056a3ea9623bd8b1341f5
Nitul Das <112264960+Nitull@users.noreply.github.com>
2024-02-08 20:30:23 +0000

    for each loops

---
+++ 05_iterations/five.js
@@ -0,0 +1,37 @@
+const coding = [ "js", "react", "nodejs", "angular" ]
+
+// coding.forEach(function (params) {
+//     console.log(params);
+    
+// })
+
+// coding.forEach( (val) => {
+//     console.log(val);
+// });
+
+// see 27 Min
+
+const myCoding = [
+    {
+        
+    languageName: "javascript",
+    languageFileName: "js"
+
+    },
+    {   
+
+    languageName: "java",
+    languageFileName: "java"
+
+    },
+    {
+
+    languageName: "python",
+    languageFileName: "py"
+
+    },
+]
+
+myCoding.forEach((item) => {
+    console.log(item.languageName);
+});
