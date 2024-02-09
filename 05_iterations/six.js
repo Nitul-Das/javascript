402172541a122458d787bc171dc5dd20d64e1df3
Nitul Das <112264960+Nitull@users.noreply.github.com>
2024-02-09 08:29:07 +0000

    filter map and reduce

---
+++ 05_iterations/six.js
@@ -0,0 +1,9 @@
+const coding = ["javascript", "react", 'mern', "node"]
+
+const value =coding.forEach((item) => {
+    console.log(item);
+    return item                                                 // for each doesnt retrun value
+
+})
+
+console.log(value);
