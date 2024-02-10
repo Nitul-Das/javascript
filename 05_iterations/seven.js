648ca357ea91a7830a7e77920c62c2e7baedeadf
Nitul Das <112264960+Nitull@users.noreply.github.com>
2024-02-10 07:30:02 +0000

    maps

--- 05_iterations/seven.js
+++ 05_iterations/seven.js
@@ -1,12 +1,19 @@
- const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+//  const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-const newNums = myNumbers.map((num) => num + 10)
-console.log(newNums);
+// const newNums = myNumbers.map((num) => num + 10)
+// console.log(newNums);
 
-const values = []
-myNumbers.forEach((num)=>{
-    values.push(num + 10);                        // using for each
-})
+// const values = []
+// myNumbers.forEach((num)=>{
+//     values.push(num + 10);                        // using for each
+// })
 
-console.log(values);
+// console.log(values);
 
+
+const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const newNumbs = myNumbers
+                .map((num) => num * 10)
+                .map((num) => num + 1)                        //second map will use the output of first map
+                .filter((num) => num <= 40)
+console.log(newNumbs);
